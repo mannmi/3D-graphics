09be38f4ea2cadca4ba7be5b5ba67d916c9c8e7f
mannmi <mirkomannigel1@gmail.com>
2023-01-13 19:55:59 +0100

    branch will go privet at some point
    branch will go privet at some point

--- OpenGL-Template-master/cmake-build-debug/launch-playground.cmd
+++
@@ -1,9 +0,0 @@
-cd F:/CLionProjects/3D-graphics/OpenGL-Template-master/playground/
-set 
-
-if [%1]==[--debugger] (
-	SHIFT
-)
-
-"F:\CLionProjects\3D-graphics\OpenGL-Template-master\cmake-build-debug\playground.exe"  
-pause
